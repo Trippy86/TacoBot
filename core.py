3f759e4478e35dded96b57641871ca50f8cb0051
l0rem <loremdevs@gmail.com>
2020-04-05 19:31:50 +0200

    Bugfixes

--- core.py
+++ core.py
@@ -1,11 +1,13 @@
 from pyrogram import Client
+from apscheduler.schedulers.background import BackgroundScheduler
 import logging
-from threading import Timer,Thread,Event
 from decouple import config
 from handlers.basic import help_handler, start_handler, store_names_handler, less_handler, delete_handler
 from handlers.leaderboards import taco_top_handler, my_tacos_handler
 from handlers.setup import self_kick_handler, new_chat_handler
 from handlers.tacotransfers import chat_reply_handler, taco_mention_handler, tacoinflator
+
+scheduler = BackgroundScheduler()
 
 
 bot_token = config('BOT_TOKEN')
@@ -15,34 +17,15 @@ api_hash = config('API_HASH')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.getLevelName(config('LOG_LEVEL', default='INFO')))
 
-bot = Client(session_name='TacoBot',
+bot = Client(session_name='TacoTestBot',
              api_id=api_id,
              api_hash=api_hash,
              bot_token=bot_token)
 
 
-class Tacoinflator():
-
-    def __init__(self, t, hFunction):
-        self.t = t
-        self.hFunction = hFunction
-        self.thread = Timer(self.t, self.handle_function)
-
-    def handle_function(self):
-        self.hFunction()
-        self.thread = Timer(self.t, self.handle_function)
-        self.thread.start()
-
-    def start(self):
-        self.thread.start()
-
-    def cancel(self):
-        self.thread.cancel()
-
-
 if __name__ == '__main__':
-    t = Tacoinflator(900, tacoinflator)
-    t.start()
+
+    scheduler.add_job(tacoinflator, trigger='cron', hour='0,12')
 
     bot.add_handler(new_chat_handler, group=-1)
     bot.add_handler(store_names_handler, group=-1)
@@ -57,8 +40,9 @@ if __name__ == '__main__':
     bot.add_handler(taco_mention_handler)
     bot.add_handler(delete_handler)
 
+    scheduler.start()
     bot.run()
 
-    logging.info("Ready and listening for updates...")
+
 
 
