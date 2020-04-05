056d51df495cc24ba6d08b184beb759f959e39da
l0rem <loremdevs@gmail.com>
2020-04-05 19:32:45 +0200

    Bugfixes

--- handlers/leaderboards.py
+++ handlers/leaderboards.py
@@ -1,12 +1,13 @@
-import json
 from decouple import config
 from pyrogram import MessageHandler, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 from dbmodels import Tacos, Chats
 from phrases import balance_phrase, balance_comment_medium, balance_comment_high, balance_comment_low,\
     taco_top_phrase, empty_top_phrase
-from chattools import get_uid, store_name, get_cid, resolve_name, get_mid, clean_chat
+from chattools import get_uid, store_name, get_cid, resolve_name, get_mid, clean_chat, ensure_no_at_sign
+
 
 default_taco_amount = config('DEFAULT_TACOS', default=50, cast=int)
+bot_username = ensure_no_at_sign(config('BOT_USERNAME', default='HeyTacoBot'))    #TODO
 
 
 def my_tacos_callback(bot, message):
@@ -17,7 +18,7 @@ def my_tacos_callback(bot, message):
 
     clean_chat(chat.mids, chat.cid, message, bot)
 
-    store_name(message)
+    user_name = store_name(message.from_user)
 
     uid = str(get_uid(message))
     tacos = Tacos.get(Tacos.chat == cid)
@@ -25,13 +26,13 @@ def my_tacos_callback(bot, message):
     ok_button = InlineKeyboardButton('OK', callback_data='delete:{}'.format(message.from_user.id))
     ok_keyboard = InlineKeyboardMarkup([[ok_button]])
 
-    balances = json.loads(tacos.taco_balance)
+    balances = tacos.taco_balance
 
     if uid in balances.keys():
         balance = balances.get(uid)
     else:
         balances.update({uid: default_taco_amount})
-        tacos.taco_balance = json.dumps(balances)
+        tacos.taco_balance = balances
         tacos.save()
         balance = default_taco_amount
 
@@ -46,19 +47,20 @@ def my_tacos_callback(bot, message):
             comment = balance_comment_medium
 
     mid = bot.send_message(chat_id=cid,
-                           text=balance_phrase.format(balance,
+                           text=balance_phrase.format(user_name,
+                                                      balance,
                                                       comment),
                            reply_to_message_id=get_mid(message),
                            reply_markup=ok_keyboard,
                            parse_mode='html').message_id
 
-    chat.mids = json.dumps([mid])
+    chat.mids = [mid]
     chat.save()
 
 
 my_tacos_handler = MessageHandler(
                                   callback=my_tacos_callback,
-                                  filters=Filters.group & Filters.command(['mytacos', 'mytacos@HeyTacoBot']))
+                                  filters=Filters.group & Filters.command(['mytacos', f'mytacos@{bot_username}']))
 
 
 def taco_top_callback(bot, message):
@@ -66,7 +68,8 @@ def taco_top_callback(bot, message):
 
     cid = get_cid(message)
     mid = get_mid(message)
-    store_name(message)
+
+    store_name(message.from_user)
 
     chat = Chats.get(Chats.cid == message.chat.id)
     clean_chat(chat.mids, chat.cid, message, bot)
@@ -76,7 +79,7 @@ def taco_top_callback(bot, message):
 
     tacos = Tacos.get(Tacos.chat == cid)
 
-    balances = json.loads(tacos.taco_balance)
+    balances = tacos.taco_balance
 
     if len(balances) == 0:                                                                # in case tacos-table is empty
         bot.send_message(text=empty_top_phrase,
@@ -114,9 +117,9 @@ def taco_top_callback(bot, message):
                            parse_mode='html',
                            disable_web_page_preview=True).message_id
 
-    chat.mids = json.dumps([mid])
+    chat.mids = [mid]
     chat.save()
 
 
 taco_top_handler = MessageHandler(callback=taco_top_callback,
-                                  filters=Filters.group & Filters.command(['tacotop', 'tacotop@HeyTacoBot']))
+                                  filters=Filters.group & Filters.command(['tacotop', f'tacotop@{bot_username}']))
